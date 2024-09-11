7866d6eaf81acf8ee87e17ee98eb219aed2055b9
Harpreet Singh <H.Singh@igglobal.com>
2024-09-11 09:00:15 +0530

    adding shell script for zap

---
+++ zap_scan.ps1
@@ -0,0 +1,18 @@
+# Download and install ZAP
+Invoke-WebRequest -Uri "https://github.com/zaproxy/zaproxy/releases/download/v2.10.0/ZAP_2_10_0_windows.exe" -OutFile "ZAP_2_10_0_windows.exe"
+Start-Process -FilePath "ZAP_2_10_0_windows.exe" -ArgumentList "/S" -Wait
+
+# Start ZAP in daemon mode
+Start-Process -FilePath "C:\Program Files\OWASP\Zed Attack Proxy\zap.bat" -ArgumentList "-daemon -port 8080" -NoNewWindow -Wait
+
+# Wait for ZAP to start
+Start-Sleep -Seconds 30
+
+# Run ZAP scan
+Invoke-WebRequest -Uri "http://localhost:8080/JSON/ascan/action/scan/?url=https://infogaingaiappformultipersona.azurewebsites.net" -Method GET
+
+# Wait for the scan to complete
+Start-Sleep -Seconds 300
+
+# Generate report
+Invoke-WebRequest -Uri "http://localhost:8080/OTHER/core/other/htmlreport/" -OutFile "zap_report.html"
